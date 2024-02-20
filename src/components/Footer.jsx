1e6ef9e979a879945ea4f17bd653c58aa9ba4bf9
Samar <osmansamar367@gmail.com>
2024-02-20 20:55:11 +0100

    Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { ReactComponent as Logo } from "../assest/logo-light.svg";
 const Footer = () => {
   return (
     <div className=" bg-[#001f3f] p-8 ">
-      <div className="flex items-center justify-between px-6">
+      <div className="flex items-center justify-between ">
         <div className="pt-2">
           {/* The ReactComponent import syntax is a special syntax used by Create
           React App for importing SVG files as React components. */}
@@ -14,7 +14,7 @@ const Footer = () => {
         {/* Button */}
         <a
           href="#Get Started"
-          className="  p-3 px-6 pt-2 text-white  bg-gradient-to-tr from-[#e534c5]  to-[#6c0b75] transition-all
+          className="  p-3 px-4 pt-2 text-white  bg-gradient-to-tr from-[#e534c5]  to-[#6c0b75] transition-all
             rounded-full self-baseline "
         >
           Get Started
