0a3a4279f747354f1c6a52405c5cea4427ebb5fa
Samar <osmansamar367@gmail.com>
2024-05-28 17:19:11 +0200

    Installed react-mouse-particles

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import Testimonails from "./components/Testimonails";
+import MouseParticles from "react-mouse-particles";
 
 function App() {
   return (
     <div>
+      <MouseParticles
+        g={2}
+        num={12}
+        color="blue"
+        cull="MuiSvgIcon-root,MuiButton-root"
+        level={8}
+      />
       <NavBar />
       <Header />
 
